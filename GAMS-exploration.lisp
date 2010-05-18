6ef6cac0fd08b9f17f008c825f28ded8cf64d347
Mathieu Lemoine <lemoinem@iro.umontreal.ca>
2010-05-18 09:17:38 -0400

    Replace positional arguments by named arguments

--- GAMS-exploration.lisp
+++ GAMS-exploration.lisp
@@ -42,100 +42,131 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 (in-package #:GAMS-exploration-script)
 
-
-;; TODO replace positional arguments by keys arguments (--args)
 ;; TODO add an argument to store lst files
-(if (= (length *arguments*) 9)
-    (destructuring-bind (initial-points-directory
-                         sets-list stop-criteria-list strategies-list
-                         model-name variable-list
-                         *set-point-loader* *initial-point-loader*
-                         solvers-list)
-        *arguments*
-      (let* ((*sets*                   (parse-sets          sets-list))
-             (stop-criteria            (parse-stop-criteria stop-criteria-list))
-             (strategies               (parse-strategies    strategies-list))
-             (*variables*              (parse-variable-list variable-list))
-             (solvers                  (parse-solver-list   solvers-list)))
-        (declare (ignore stop-criteria strategies))
-        (let* ((min-result) (max-result))
-          (loop named main-loop
-             with feasible-point-found-p = t
+(block nil
+  (destructuring-bind (*lst-directory*
+                       min-file max-file
+                       initial-points-directory 
+                       sets-list stop-criteria-list strategies-list
+                       model-name variable-list
+                       *set-point-loader* *initial-point-loader*
+                       solvers-list)
+      (let (args (mapcar #'get-argument '("--lsts" "--min" "--max"
+                                          "--init-points" "--sets"
+                                          "--stops" "--strategies"
+                                          "--gms" "--variables"
+                                          "--current-sets"
+                                          "--current-point"
+                                          "--solvers")))
+        (when (or (some #'null args)
+                  (get-argument "--help" :boolean-p))
+          (format *error-output*
+                  "Mandatory arguments:
+--init-points
+~Tdirectory in which initial points will be stored.
+--sets
+~Tlist of sets of indices to explore
+--stops
+~Tlist of stop criteria
+--strategies
+~Tlist of strategies
+--gms
+~TGAMS model
+--variables
+~Tlist of variables in the GAMS model
+--current-sets
+~Tfile in which the current set point will be stored
+--current-point
+~Tfile in which the current start point will be stored
+--solvers
+~Tlist of solvers directives
+--help
+~Tdisplay this help message then quit
+Current args:~%~A" *arguments*)
+          (return))
+        args)
+    (let* ((*sets*                   (parse-sets          sets-list))
+           (stop-criteria            (parse-stop-criteria stop-criteria-list))
+           (strategies               (parse-strategies    strategies-list))
+           (*variables*              (parse-variable-list variable-list))
+           (solvers                  (parse-solver-list   solvers-list)))
+      (declare (ignore stop-criteria strategies))
+      (let* ((min-result) (max-result))
+        (loop named main-loop
+           with feasible-point-found-p = t
 
-             for previous-result-points = nil
-             then (apply #'aref result-points result-point-coordinates)
+           for previous-result-points = nil
+           then (apply #'aref result-points result-point-coordinates)
 
-             for (set-point current-set) = (list (make-set-point))
-             then (let ((*sets* (if (or feasible-point-found-p
-                                        (null current-set)
-                                        (< (gethash current-set set-point)
-                                           (dynamic-set-min-size current-set)))
-                                    *sets*
-                                    (remove current-set *sets*))))
-                    (multiple-value-list
-                     (generate-next-set-point set-point result-points)))
+           for (set-point current-set) = (list (make-set-point))
+           then (let ((*sets* (if (or feasible-point-found-p
+                                      (null current-set)
+                                      (< (gethash current-set set-point)
+                                         (dynamic-set-min-size current-set)))
+                                  *sets*
+                                  (remove current-set *sets*))))
+                  (multiple-value-list
+                   (generate-next-set-point set-point result-points)))
 
-             for result-points = (make-array (set-point->result-known-dimensions set-point)
-                                             :element-type 'list :initial-element nil
-                                             :adjustable t)
-             then (if set-point
-                      (adjust-array result-points (set-point->result-known-dimensions set-point)
-                                    :initial-element nil)
-                      result-points)
+           for result-points = (make-array (set-point->result-known-dimensions set-point)
+                                           :element-type 'list :initial-element nil
+                                           :adjustable t)
+           then (if set-point
+                    (adjust-array result-points (set-point->result-known-dimensions set-point)
+                                  :initial-element nil)
+                    result-points)
 
-             for result-point-coordinates = (when set-point
-                                              (set-point->result-coordinate set-point))
+           for result-point-coordinates = (when set-point
+                                            (set-point->result-coordinate set-point))
 
-             for initial-points = (when set-point
-                                    (let ((stage (cond
-                                                   ((null current-set) :empty-set)
-                                                   ((= (gethash current-set set-point) 1) :first-element)
-                                                   (t :additional-element))))
-                                      (generate-initial-points set-point stage
-                                                               current-set previous-result-points)))
+           for initial-points = (when set-point
+                                  (let ((stage (cond
+                                                 ((null current-set) :empty-set)
+                                                 ((= (gethash current-set set-point) 1) :first-element)
+                                                 (t :additional-element))))
+                                    (generate-initial-points set-point stage
+                                                             current-set previous-result-points)))
 
-             while set-point
+           while set-point
 
-             do (load-set-point set-point)
-               (map 'nil (rcurry #'write-initial-point initial-points-directory) initial-points)
-               (setq feasible-point-found-p nil)
-               (flet ((add-result (result)
-                        (push result (apply #'aref result-points result-point-coordinates))
-                        (when (feasible-point-p result)
-                          (setq feasible-point-found-p t)
-                          (when (or (null min-result)
-                                    (< (result-point-objective-value result)
-                                       (result-point-objective-value min-result)))
-                            (setq min-result result))
-                          (when (or (null max-result)
-                                    (> (result-point-objective-value result)
-                                       (result-point-objective-value max-result)))
-                            (setq max-result result)))))
-                 (map 'nil
-                      (lambda (initial-point)
-                        (declare (type initial-point initial-point))
-                        (load-initial-point initial-point)
-                        (map 'nil
-                             (lambda (solver)
-                               (add-result
-                                (solve-GAMS-model model-name initial-point solver
-                                                  #'store-GAMS-error)))
-                             solvers))
-                      initial-points))
+           do (load-set-point set-point)
+             (map 'nil (rcurry #'write-initial-point initial-points-directory) initial-points)
+             (setq feasible-point-found-p nil)
+             (flet ((add-result (result)
+                      (push result (apply #'aref result-points result-point-coordinates))
+                      (when (feasible-point-p result)
+                        (setq feasible-point-found-p t)
+                        (when (or (null min-result)
+                                  (< (result-point-objective-value result)
+                                     (result-point-objective-value min-result)))
+                          (setq min-result result))
+                        (when (or (null max-result)
+                                  (> (result-point-objective-value result)
+                                     (result-point-objective-value max-result)))
+                          (setq max-result result)))))
+               (map 'nil
+                    (lambda (initial-point)
+                      (declare (type initial-point initial-point))
+                      (load-initial-point initial-point)
+                      (map 'nil
+                           (lambda (solver)
+                             (add-result
+                              (solve-GAMS-model model-name initial-point solver
+                                                #'store-GAMS-error)))
+                           solvers))
+                    initial-points))
 
-             finally (return-from main-loop result-points))
-          (format t "**** MINIMAL SOLUTION ****~%")
-          (print-result-point min-result)
-          (format t "**** MAXIMAL SOLUTION ****~%")
-          (print-result-point max-result)
-          (when *GAMS-errors*
-            (format t "**** ERROR POINTS ****~%~%")
-            (map 'nil (lambda (error-point)
-                        (destructuring-bind (exit-code GAMS-model initial-point solvers) error-point
-                          (format t "exit-code: ~A~%model: ~A~%solvers: ~A~%initial point: ~A {{~A}}~%~%"
-                                  exit-code GAMS-model solvers
-                                  (initial-point-file-name initial-point)
-                                  (initial-point-history   initial-point))))
-                 *GAMS-errors*)))))
-    (unless (null *arguments*)
-      (error "This script requires 9 arguments: initial-points-directory exploration stop-criteria strategies GAMS-model.gms variables.inc set-point.inc initializer.inc solvers~%~A" *arguments*)))
+           finally (return-from main-loop result-points))
+        (format t "**** MINIMAL SOLUTION ****~%")
+        (print-result-point min-result)
+        (format t "**** MAXIMAL SOLUTION ****~%")
+        (print-result-point max-result)
+        (when *GAMS-errors*
+          (format t "**** ERROR POINTS ****~%~%")
+          (map 'nil (lambda (error-point)
+                      (destructuring-bind (exit-code GAMS-model initial-point solvers) error-point
+                        (format t "exit-code: ~A~%model: ~A~%solvers: ~A~%initial point: ~A {{~A}}~%~%"
+                                exit-code GAMS-model solvers
+                                (initial-point-file-name initial-point)
+                                (initial-point-history   initial-point))))
+               *GAMS-errors*))))))
